257990a774b1b7d1563f8bc0ea2333d81f178cef
youswim <youswim96@gmail.com>
2023-12-31 00:22:30 +0900

    styled-component 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyStyled>hello React</MyStyled>
     </div>
   );
 }
+
+const MyStyled = styled.div`
+  width: 50vw;
+  min-height: 150px;
+  padding: 10px;
+  border-radius: 15px;
+  color: #fff;
+  &:hover {
+    background-color: #ddd;
+  }
+  background-color: yellow;
+`;
 
 export default App;
