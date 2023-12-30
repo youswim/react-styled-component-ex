33fe9eb7564da0b8c851dd754fb32e303e603463
youswim <youswim96@gmail.com>
2023-12-31 00:29:13 +0900

    styled-component에 삼항연산자 사용

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import styled from 'styled-components'
 function App() {
   return (
     <div className="App">
+      {/*props가 있으므로 배경이 red*/}
       <MyStyled bgColor={"Red"}>hello React</MyStyled>
+      {/*props가 없으므로 배경이 purple*/}
+      <MyStyled>hello React</MyStyled>
     </div>
   );
 }
@@ -18,7 +21,7 @@ const MyStyled = styled.div`
   &:hover {
     background-color: #ddd;
   }
-  background-color: ${(props) => (props.bgColor)};
+  background-color: ${(props) => (props.bgColor ? "red" : "purple")};
 `;
 
 export default App;
